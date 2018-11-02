df6142932cf15c3080766797f1f7aae2df23b9f6
Ashish Shukla <v-shuash@microsoft.com>
2018-11-02 15:13:13 +0530

    Initial Commit

---
+++ dattingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using dattingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dattingApp.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
